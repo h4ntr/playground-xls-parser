939b2951d49ce9ca2657d8c30627c7f04bd87c9d
h4ntr <31856135+h4ntr@users.noreply.github.com>
2017-09-21 15:35:46 +0300

    Play around the code. Nothing special.

--- main.go
+++ main.go
@@ -1,89 +1,95 @@
 package main
 
 import (
-    "encoding/csv"
-    "fmt"
-    "os"
-    "log"
-    "io"
-    //"bufio"
-    "github.com/ctessum/macreader"
-    "strings"
+	"encoding/csv"
+	"fmt"
+	"os"
+	"log"
+	"io"
+	"github.com/ctessum/macreader"
+	"strings"
+	"regexp"
 )
 
-const sSourceFileName = "/Users/svengerskiy/Documents/RingCentral/hr/people/PeopleTest2017-09-12.csv"
+const sSourceFileName = "./test.csv"
 
-func parseHistory(s string) {
-    ss := strings.Split(s,",")
-    for _, sChange := range ss {
-        sChange = strings.TrimSpace(sChange)
-        fmt.Println("-------->",sChange)
-    }
+func ParseToken(s string){
+	const sBucksExp = `^\$([\d](.\d){0,1})k$`
+	var BucksExp = regexp.MustCompile(sBucksExp)
+	var doesMatch = BucksExp.MatchString(s)
+	if doesMatch {
+		fmt.Printf("Token \"%v\" matches `%v` = %v\n", s, sBucksExp, doesMatch)
+		fmt.Printf("Submatch: \"%q\"\n\n", BucksExp.FindAllStringSubmatch(s,-1))
+	}
 }
 
-func main(){
+func ParseChangeRecord(s string){
+	ss := strings.Split(s, " ")
+	for _, sToken := range ss{
+		ParseToken(strings.Trim(sToken," \t"))
+	}
+}
 
-    fmt.Println(">>I'll try to do a little bit of parsing for you now...")
+func ParseHistory(s string) {
+	ss := strings.Split(s, ";")
+	for _, sChgRecord := range ss {
+		ParseChangeRecord(sChgRecord)
+	}
+}
 
-    // I can use Cobra (https://github.com/spf13/cobra) to create a nice CLI later.
-    // A simpler implementation may be achieved with package "flag"
+func main() {
 
-    // Open file and pass it to the csv parser.
-    // Split lines into fields, one of them History
-    f, err := os.Open(sSourceFileName)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer f.Close()
-/*
-    scanner := bufio.NewScanner(f)
-    for scanner.Scan(){
-        s := scanner.Text()
-        fmt.Println(s[0:30])
-    }
-    if err := scanner.Err(); err != nil {
-        log.Fatal(err)
-    }
-*/
+	fmt.Println(">>I'll try to do a little bit of parsing for you now...")
 
-    csvReader := csv.NewReader(macreader.New(f))
+	// I can use Cobra (https://github.com/spf13/cobra) to create a nice CLI later.
+	// A simpler implementation may be achieved with package "flag"
 
-    var headers []string
-    for lineNum := 1; ; lineNum++ {
+	// Open file and pass it to the csv parser.
+	// Split lines into fields, one of them History
+	f, err := os.Open(sSourceFileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
 
-        line, err := csvReader.Read()
-        if err == io.EOF {
-            fmt.Println(">>> EOF reached.")
-            break
-        } else if err != nil {
-            log.Fatal(err)
-        }
+	csvReader := csv.NewReader(macreader.New(f))
 
-        fmt.Printf("\nLine %v:\n", lineNum)
+	var headers []string
+	for lineNum := 1; ; lineNum++ {
 
-        if lineNum == 1 {
-            headers = line
-        }
+		line, err := csvReader.Read()
+		if err == io.EOF {
+			fmt.Println(">>> EOF reached.")
+			break
+		} else if err != nil {
+			log.Fatal(err)
+		}
 
-        // No error checks starting from here. Need to be added
+		fmt.Printf("\nLine %v:\n", lineNum)
 
-        for col := 0; col < len(line); col++ {
-            if lineNum == 1 {
-                fmt.Printf("\t%v\n", headers[col])
-            } else {
-                fmt.Printf("\t%v: \"%v\"\n", headers[col], line[col])
-                if headers[col] == "History" {
-                    parseHistory(line[col])
-                }
-            }
-        }
-    }
+		if lineNum == 1 {
+			headers = line
+		}
 
-    // Parse History fields using regular experssions recgonizing
-    // change dates, sums, currencies, and titles
+		// No error checks starting from here. Need to be added
 
-    // Save it. Where?
-    // Join with historical exchanges rates. How?
-    // Build charts. How?
-    // Add filters, dimensions. Which and how?
+		for col := 0; col < len(line); col++ {
+			if lineNum == 1 {
+				fmt.Printf("\t%v\n", headers[col])
+			} else {
+				fmt.Printf("\t%v: \"%v\"\n", headers[col], line[col])
+				if headers[col] == "History" {
+					ParseHistory(line[col])
+				}
+			}
+		}
+	}
+
+	// Parse History fields using regular experssions recgonizing
+	// change dates, sums, currencies, and titles
+
+	// Save it. Where?
+	// Join with historical exchanges rates. How?
+	// Build charts. How?
+	// Add filters, dimensions. Which and how?
 }
